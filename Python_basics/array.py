b2365bb85060a940158d7f2aa8b304118b076c3f
Sunghan Kim <53588299+suka35@users.noreply.github.com>
2021-04-16 19:45:03 +0900

    Create array.py

---
+++ Python_basics/array.py
@@ -0,0 +1,13 @@
+# 행렬 곱셈
+import numpy as np
+a = np.matrix([[1, 2], [4, 5], [7, 8]]) # 2*3
+b = np.matrix([[2, 0, 0], [0, 2, 0]]) # 3 * 2
+a * b
+b * a
+
+
+def solution(arr1, arr2):
+    a = np.matrix([[1, 2], [4, 5], [7, 8]]) # 2*3
+    b = np.matrix([[2, 0, 0], [0, 2, 0]]) # 3 * 2
+    answer = a * b
+    return answer
